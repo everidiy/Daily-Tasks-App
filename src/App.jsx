de9600ae962e70edc6eb01d41612549b2d5f727a
everidiy <itusitas@gmail.com>
2025-12-15 20:46:59 +0300

    yt v 0.9

--- src/App.jsx
+++ src/App.jsx
@@ -39,8 +39,17 @@ function App() {
     setDragged(null);
   };
 
+  const date = new Date();
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+
   return (
     <div className="board-wrapper">
+      <div className="date">
+          <p>{day}.{month}.{year}</p>
+      </div>
+
       <TodayBoard 
         tasks={tasks}
         deleteTask={deleteTask}
